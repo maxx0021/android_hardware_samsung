4f635e6a75735dfba99f9514462c0d7d50a662b8
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2022-02-23 14:35:19 +0530

    Update Android.mk

--- ril/libril/Android.mk
+++ ril/libril/Android.mk
@@ -1,6 +1,6 @@
 # Copyright 2006 The Android Open Source Project
 
-ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+ifneq ($(BOARD_PROVIDES_LIBRIL),true)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -21,46 +21,29 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy \
     librilutils \
     android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
     android.hardware.radio.deprecated@1.0 \
-    libhidlbase
+    libhidlbase \
 
 LOCAL_STATIC_LIBRARIES := \
-    libprotobuf-c-nano-enable_malloc \
+    libprotobuf-c-nano-enable_malloc-32bit \
 
-LOCAL_CFLAGS += -Wno-unused-parameter
+LOCAL_CFLAGS += -Wall -Wextra -Wno-unused-parameter -Werror
+LOCAL_CFLAGS += -DPB_FIELD_32BIT
 
 ifeq ($(SIM_COUNT), 2)
     LOCAL_CFLAGS += -DANDROID_MULTI_SIM -DDSDA_RILD1
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
 
-ifneq ($(filter xmm6262 xmm6360,$(BOARD_MODEM_TYPE)),)
-LOCAL_CFLAGS += -DMODEM_TYPE_XMM6262
-endif
-ifeq ($(BOARD_MODEM_TYPE),xmm6260)
-LOCAL_CFLAGS += -DMODEM_TYPE_XMM6260
-endif
-ifneq ($(filter m7450 mdm9x35 ss333 tss310 xmm7260,$(BOARD_MODEM_TYPE)),)
-LOCAL_CFLAGS += -DSAMSUNG_NEXT_GEN_MODEM
-endif
-
-ifeq ($(BOARD_MODEM_NEEDS_VIDEO_CALL_FIELD), true)
-LOCAL_CFLAGS += -DNEEDS_VIDEO_CALL_FIELD
-endif
-
-ifeq ($(BOARD_NEEDS_ROAMING_PROTOCOL_FIELD), true)
-LOCAL_CFLAGS += -DNEEDS_ROAMING_PROTOCOL_FIELD
-endif
-
-ifeq ($(BOARD_NEEDS_IMS_TYPE_FIELD), true)
-LOCAL_CFLAGS += -DNEEDS_IMS_TYPE_FIELD
-endif
-
 ifneq ($(DISABLE_RILD_OEM_HOOK),)
     LOCAL_CFLAGS += -DOEM_HOOK_DISABLED
 endif
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+ifneq ($(TARGET_USES_OLD_MNC_FORMAT),)
+    LOCAL_CFLAGS += -DOLD_MNC_FORMAT
+endif
+
 LOCAL_C_INCLUDES += external/nanopb-c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
